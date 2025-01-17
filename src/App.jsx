38efe626387758c50a6f9c126b5fbbe513a2cbed
Daniel Yllanes <danielyllanes@Daniels-MacBook-Air.local>
2025-01-17 02:36:58 -0500

    fcc React19 section 3 lesson 16 toggling state completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-
+import {useState} from 'react'
 function App() {
 
+  const [isGoingOut, setIsGoingOut] = useState(false)
+
+  function handleClick(){
+    setIsGoingOut(!isGoingOut)
+  }
   return (
     <main>
       <div>
         <h1>Do I feel like going out tonight?</h1>
-        <button>Yes</button>
+        <button 
+                onClick={handleClick}
+                aria-label={`Current answer is ${isGoingOut?'Yes':'No'}. Click to change it.`}        
+        >
+          {isGoingOut?'Yes':'No'}
+        </button>
       </div>
     </main>
   )
