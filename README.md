975ca51400894f472b8e5346efd7c4360ddd4ff3
Daniel Yllanes <danielyllanes@Daniels-MacBook-Air.local>
2025-01-17 02:20:33 -0500

    lesson starter code

---
+++ README.md
@@ -0,0 +1,4 @@
+fcc React 19
+Section 3
+Lesson 16
+Toggling State
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <main>
+      <div>
+        <h1>Do I feel like going out tonight?</h1>
+        <button>Yes</button>
+      </div>
+    </main>
+  )
+}
+
+export default App
